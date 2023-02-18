1118e08233b037c50f0617331da31ea56ea13003
Benni Adham <benni.adham@gmail.com>
2023-02-19 06:20:56 +0700

    fmt_string test unit

---
+++ tests/fmt_string.cpp
@@ -0,0 +1,29 @@
+#include <ltd.hpp>
+
+#include <string>
+
+using namespace ltd;
+
+auto main(int argc, char** argv) -> int
+{
+    test_unit tu;
+
+    tu.test([&tu](){
+        std::string result = fmt::sprintf("'%s'", "string");
+        tu.expect(result, "'string'");
+    });
+
+    tu.test([&tu](){
+        std::string result = fmt::sprintf("'%10s'", "string");
+        tu.expect(result, "'    string'");
+    });
+
+    tu.test([&tu](){
+        std::string result = fmt::sprintf("'%-10s'", "string");
+        tu.expect(result, "'string    '");
+    });
+    
+    tu.run(argc, argv);
+    
+    return 0;
+}
